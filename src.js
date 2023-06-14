6e28d5a3c6929ede95788b6def6ab27b71a2ece2
assad2000 <44008093+assad2000@users.noreply.github.com>
2023-06-14 02:26:17 +0100

    Update src.js

--- src.js
+++ src.js
@@ -1,50 +1,53 @@
-
-
 buttonToff = document.querySelector('#timeoff');
 buttonTclock = document.querySelector('#timeclock');
 buttonTword = document.querySelector('#timeword');
 buttonTout = document.querySelector('#timeout');
 
 pageTimeOff = document.querySelector('.pagetimeoff');
+pageTimeWord = document.querySelector('.pagetimeword');
+pageTimeClock = document.querySelector('.pagetimeclock');
+pageTimeOut = document.querySelector('.pagetimeout');
 
-
+clickBar = 'off';
+arPages = [pageTimeClock,pageTimeWord,pageTimeOff,pageTimeOut];
+arButt = [buttonTclock,buttonTword,buttonToff,buttonTout];
 
 
 
 
 
 buttonToff.addEventListener('click', function(){
-  
-  pageTimeOff.style.display = 'block';
-  buttonToff.style.borderBottom = '2px solid black';
-  buttonTout.style.border = 'none';
-  buttonTword.style.border = 'none';
-  buttonTclock.style.border = 'none';
+  page('off');
 })
 
 buttonTclock.addEventListener('click', function(){
-  
-  pageTimeOff.style.display = 'none';
-  buttonTclock.style.borderBottom = '2px solid black';
-  buttonTout.style.border = 'none';
-  buttonTword.style.border = 'none';
-  buttonToff.style.border = 'none';
+  page('clock');
 })
 
 buttonTword.addEventListener('click', function(){
-  
-  pageTimeOff.style.display = 'none';
-  buttonTword.style.borderBottom = '2px solid black';
-  buttonTout.style.border = 'none';
-  buttonToff.style.border = 'none';
-  buttonTclock.style.border = 'none';
+  page('word');
 })
 
 buttonTout.addEventListener('click', function(){
-  
-  pageTimeOff.style.display = 'none';
-  buttonTout.style.borderBottom = '2px solid black';
-  buttonToff.style.border = 'none';
-  buttonTword.style.border = 'none';
-  buttonTclock.style.border = 'none';
+  page('out');
 })
+
+function page(clickBar){
+  for (var t = 0; t < arPages.length; t++) {
+    arPages[t].style.display ='none';
+    arButt[t].style.borderBottom = 'none';
+  }
+  if (clickBar == 'off') {
+    pageTimeOff.style.display = 'block';
+    buttonToff.style.borderBottom = '2px solid black';
+  } else if (clickBar == 'out') {
+    pageTimeOut.style.display = 'block';
+    buttonTout.style.borderBottom = '2px solid black';
+  } else if (clickBar == 'word') {
+    pageTimeWord.style.display = 'block';
+    buttonTword.style.borderBottom = '2px solid black';
+  } else if (clickBar == 'clock') {
+    pageTimeClock.style.display = 'block';
+    buttonTclock.style.borderBottom = '2px solid black';
+  }
+}
